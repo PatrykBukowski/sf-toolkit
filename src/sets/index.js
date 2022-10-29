041658d80616176b6a8c510809bd14a8a245bf77
Patryk Bukowski <patryk.bukowski@sobre.pl>
2022-10-29 10:47:46 +0200

    Added prettier

--- src/sets/index.js
+++ src/sets/index.js
@@ -1,29 +1,34 @@
 const difference = (arr1, arr2) => {
-  const arr1Elements = arr1.xmlf('CustomLabels').elements
-  const arr2Elements = arr2.xmlf('CustomLabels').elements
+  const arr1Elements = arr1.xmlf("CustomLabels").elements;
+  const arr2Elements = arr2.xmlf("CustomLabels").elements;
 
-  return arr1Elements.filter(element1 => {
-    return !arr2Elements.some(element2 => {
-      const fullName1 = element1.xmlf('fullName').xmlf('text').text
-      const fullName2 = element2.xmlf('fullName').xmlf('text').text
-      return fullName1 === fullName2
-    })
-  })
-}
-
-
+  return arr1Elements.filter((element1) => {
+    return !arr2Elements.some((element2) => {
+      const fullName1 = element1.xmlf("fullName").xmlf("text").text;
+      const fullName2 = element2.xmlf("fullName").xmlf("text").text;
+      return fullName1 === fullName2;
+    });
+  });
+};
 
 const intersection = (arr1, arr2) => {
-  const arr1Elements = arr1.xmlf('CustomLabels').elements
-  const arr2Elements = arr2.xmlf('CustomLabels').elements
+  const arr1Elements = arr1.xmlf("CustomLabels").elements;
+  const arr2Elements = arr2.xmlf("CustomLabels").elements;
 
-  return arr1Elements.filter(element1 => {
-    return arr2Elements.some(element2 => {
-      const fullName1 = element1.xmlf('fullName').xmlf('text').text
-      const fullName2 = element2.xmlf('fullName').xmlf('text').text
-      return fullName1 === fullName2
-    })
-  })
-}
+  return arr1Elements.filter((element1) => {
 
-export {difference, intersection}
+
+
+    return arr2Elements.some((element2) => {
+      const fullName1 = element1.xmlf("fullName").xmlf("text").text;
+
+
+
+
+      const fullName2 = element2.xmlf("fullName").xmlf("text").text;
+      return fullName1 === fullName2;
+    });
+  });
+};
+
+export { difference, intersection };
