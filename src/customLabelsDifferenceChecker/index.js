a31d904ee203c6b29b47f6a0d40dfc4286664d3b
Patryk Bukowski <patryk.bukowski@sobre.pl>
2022-10-29 10:45:49 +0200

    Added prettier

--- src/customLabelsDifferenceChecker/index.js
+++ src/customLabelsDifferenceChecker/index.js
@@ -1,80 +1,91 @@
-import {read, write} from "../utils/fs.js";
-import {build, parse} from '../utils/xml.js';
-import {templateAll, templateMain} from "../templates/index.js";
-import {difference, intersection} from "../sets/index.js";
-import {flagger} from "../utils/flagger.js";
+import { read, write } from "../utils/fs.js";
+import { build, parse } from "../utils/xml.js";
+import { templateAll, templateMain } from "../templates/index.js";
+import { difference, intersection } from "../sets/index.js";
+import { flagger } from "../utils/flagger.js";
 import xmlf from "../utils/xmlf.js";
 
 const CustomLabelDifferenceChecker = () => {
-  const filename1 = flagger('-in1', 'file1.xml');
-  const filename2 = flagger('-in2', 'file2.xml');
-  const mode = flagger('-m', 'all');
-  const outputname1 = flagger('-o1', mode === 'allInOne' ? 'output.xml' : 'correct.xml');
-  const outputname2 = flagger('-o2', 'missingInA.xml');
-  const outputname3 = flagger('-o3', 'missingInB.xml');
-  const outputname4 = flagger('-o4', 'incorrect.xml');
+  const filename1 = flagger("-in1", "file1.xml");
+  const filename2 = flagger("-in2", "file2.xml");
+  const mode = flagger("-m", "all");
+  const outputname1 = flagger(
+    "-o1",
+    mode === "allInOne" ? "output.xml" : "correct.xml"
+  );
+  const outputname2 = flagger("-o2", "missingInA.xml");
+  const outputname3 = flagger("-o3", "missingInB.xml");
+  const outputname4 = flagger("-o4", "incorrect.xml");
   Object.prototype.xmlf = xmlf;
 
-  const fileA = parse(read(filename1))
-  const fileB = parse(read(filename2))
+  const fileA = parse(read(filename1));
+  const fileB = parse(read(filename2));
 
-
-  let correct
-  let missingInA
-  let missingInB
-  let incorrectA = []
-  let incorrectB = []
+  let correct;
+  let missingInA;
+  let missingInB;
+  let incorrectA = [];
+  let incorrectB = [];
   let incorrect = [];
-
 
   const check = () => {
     const intersection1 = intersection(fileA, fileB);
     for (const elementA of intersection1) {
-      const elementB = fileB.xmlf('CustomLabels').elements.find(el => {
-        const fullNameA = elementA.xmlf('fullName').xmlf('text').text
-        const fullNameB = el.xmlf('fullName').xmlf('text').text;
-        return fullNameA === fullNameB
+      const elementB = fileB.xmlf("CustomLabels").elements.find((el) => {
+        const fullNameA = elementA.xmlf("fullName").xmlf("text").text;
+        const fullNameB = el.xmlf("fullName").xmlf("text").text;
+        return fullNameA === fullNameB;
       });
-      const valueA = elementA.xmlf('value').xmlf('text').text;
-      const valueB = elementB.xmlf('value').xmlf('text').text;
+      const valueA = elementA.xmlf("value").xmlf("text").text;
+      const valueB = elementB.xmlf("value").xmlf("text").text;
       if (valueA === valueB) {
-        correct = elementA
+        correct = elementA;
       } else {
-        incorrectA.push(elementA)
-        incorrectB.push(elementB)
+        incorrectA.push(elementA);
+        incorrectB.push(elementB);
       }
     }
-    missingInA = difference(fileB, fileA)
-    missingInB = difference(fileA, fileB)
+    missingInA = difference(fileB, fileA);
+    missingInB = difference(fileA, fileB);
 
     for (const [index, value] of incorrectA.entries()) {
       const tagA = {
-        type: 'element', name: 'valueA', elements: incorrectA[index].elements.xmlf('value').elements
-      }
+        type: "element",
+        name: "valueA",
+        elements: incorrectA[index].elements.xmlf("value").elements,
+      };
       const tagB = {
-        type: 'element', name: 'valueB', elements: incorrectB[index].elements.xmlf('value').elements
-      }
+        type: "element",
+        name: "valueB",
+        elements: incorrectB[index].elements.xmlf("value").elements,
+      };
       incorrect.push({
-        type: value.type, name: value.name, elements: [value.elements.xmlf('fullName'), tagA, tagB,]
-      })
+        type: value.type,
+        name: value.name,
+        elements: [value.elements.xmlf("fullName"), tagA, tagB],
+      });
     }
-  }
+  };
   check();
 
-  if (
-    mode === 'allInOne'
-  ) {
-    write(outputname1, build(templateAll({
-      correct: [correct], missingInA: missingInA, missingInB: missingInB, incorrect: incorrect
-    })))
+  if (mode === "allInOne") {
+    write(
+      outputname1,
+      build(
+        templateAll({
+          correct: [correct],
+          missingInA: missingInA,
+          missingInB: missingInB,
+          incorrect: incorrect,
+        })
+      )
+    );
   } else {
-    write(outputname1, build(templateMain([correct])))
-    write(outputname2, build(templateMain(missingInA)))
-    write(outputname3, build(templateMain(missingInB)))
-    write(outputname4, build(templateMain(incorrect)))
+    write(outputname1, build(templateMain([correct])));
+    write(outputname2, build(templateMain(missingInA)));
+    write(outputname3, build(templateMain(missingInB)));
+    write(outputname4, build(templateMain(incorrect)));
   }
+};
 
-
-}
-
-export default CustomLabelDifferenceChecker
+export default CustomLabelDifferenceChecker;
--- src/profileSortingTool/index.js
+++ src/profileSortingTool/index.js
@@ -1,55 +1,55 @@
-import {read, write} from "../utils/fs.js";
-import {build, parse} from '../utils/xml.js';
-import {flagger} from "../utils/flagger.js";
+import { read, write } from "../utils/fs.js";
+import { build, parse } from "../utils/xml.js";
+import { flagger } from "../utils/flagger.js";
 import xmlf from "../utils/xmlf.js";
 
 const groupBy = (xs, key) => {
   return xs.reduce((rv, x) => {
     (rv[x[key]] = rv[x[key]] || []).push(x);
     return rv;
-  }, {})
-}
+  }, {});
+};
 
 const SORT_KEYWORDS = {
-  applicationVisibilities: 'application',
-  categoryGroupVisibilities: 'dataCategoryGroup',
-  classAccesses: 'apexClass',
-  customSettingAccesses:'name',
-  fieldPermissions: 'field',
-  flowAccesses: 'flow',
-  layoutAssignments: 'layout',
-  objectPermissions: 'object',
-  pageAccesses: 'apexPage',
-  recordTypeVisibilities: 'recordType',
-  tabVisibilities: 'tab'
-}
+  applicationVisibilities: "application",
+  categoryGroupVisibilities: "dataCategoryGroup",
+  classAccesses: "apexClass",
+  customSettingAccesses: "name",
+  fieldPermissions: "field",
+  flowAccesses: "flow",
+  layoutAssignments: "layout",
+  objectPermissions: "object",
+  pageAccesses: "apexPage",
+  recordTypeVisibilities: "recordType",
+  tabVisibilities: "tab",
+};
 
 const ProfileSortingTool = () => {
-  const inputFile = flagger('-in', 'Admin.profile-meta.xml');
-  const outputFile = flagger('-out', 'Admin.profile-meta.xml');
+  const inputFile = flagger("-in", "Admin.profile-meta.xml");
+  const outputFile = flagger("-out", "Admin.profile-meta.xml");
   const file = parse(read(inputFile));
   Object.prototype.xmlf = xmlf;
 
-  const allLabels = file.xmlf('Profile').elements
+  const allLabels = file.xmlf("Profile").elements;
 
-  const group = groupBy(allLabels, 'name');
+  const group = groupBy(allLabels, "name");
   let newGroup = [];
-  for(const index in group){
-    if(index === 'xmlf') continue;
-    if(!SORT_KEYWORDS[index]) {
+  for (const index in group) {
+    if (index === "xmlf") continue;
+    if (!SORT_KEYWORDS[index]) {
       newGroup.push(...group[index]);
       continue;
     }
     const masterTag = group[index];
     masterTag.sort((elementA, elementB) => {
-      const a = elementA.elements.xmlf(SORT_KEYWORDS[index]).xmlf('text').text;
-      const b = elementB.elements.xmlf(SORT_KEYWORDS[index]).xmlf('text').text;
+      const a = elementA.elements.xmlf(SORT_KEYWORDS[index]).xmlf("text").text;
+      const b = elementB.elements.xmlf(SORT_KEYWORDS[index]).xmlf("text").text;
       return a.localeCompare(b);
-    })
+    });
     newGroup.push(...group[index]);
   }
-  file.xmlf('Profile').elements = newGroup
-  write(outputFile, build(file))
-}
+  file.xmlf("Profile").elements = newGroup;
+  write(outputFile, build(file));
+};
 
-export default ProfileSortingTool
+export default ProfileSortingTool;
